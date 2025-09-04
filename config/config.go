125fbc3f751d2308579b66709e9b65775e547150
mehedi <mahbub@evidentbd.com>
2025-09-05 01:10:33 +0600

    add google redirect url

--- config/config.go
+++ config/config.go
@@ -13,7 +13,7 @@ func InitGoogleAuth() {
 	GoogleOAuthConfig = &oauth2.Config{
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		RedirectURL:  "",
+		RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
 		Scopes: []string{
 			"https://www.googleapis.com/auth/userinfo.email",
 			"https://www.googleapis.com/auth/userinfo.profile",
